85cac3879d5e4d682c9a6cddc8c90b19ea9123d5
P4ralaxz <paralaxz477@gmail.com>
2024-10-18 20:15:17 +0700

    OCR

--- main.py
+++ main.py
@@ -16,3 +16,34 @@ def sort_cont(character_contours):
     (character_contours, boundingBoxes) = zip(*sorted(zip(character_contours,boundingBoxes),key = lambda b: b[1][i],reverse = False))
        
     return character_contours
+
+
+
+
+class OCR:
+       
+    def __init__(self, modelFile, labelFile): 
+           
+        self.model_file = modelFile 
+        self.label_file = labelFile 
+        self.label = self.load_label(self.label_file) 
+        self.graph = self.load_graph(self.model_file) 
+        self.sess = tf.compat.v1.Session(graph=self.graph,  
+                                         config=tf.compat.v1.ConfigProto()) 
+   
+    def load_graph(self, modelFile): 
+           
+        graph = tf.Graph() 
+        graph_def = tf.compat.v1.GraphDef() 
+           
+        with open(modelFile, "rb") as f: 
+            graph_def.ParseFromString(f.read()) 
+           
+        with graph.as_default(): 
+            tf.import_graph_def(graph_def) 
+           
+        return graph 
+   
+    def load_label(self, labelFile): 
+        label = [] 
+        proto_as_ascii_lines = tf.io.gfile.GFile(labelFile).readlines()
