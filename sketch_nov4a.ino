9b9662bf38d021a54705963dad97d60abc601350
nippitch239 <67070092@kmitl.ac.th>
2024-11-06 17:57:08 +0700

    lastUpdate

--- sketch_nov4a.ino
+++ sketch_nov4a.ino
@@ -1,27 +1,23 @@
 #include <Servo.h>
 
-Servo myServo;  // Create a Servo object
-const int servoPin = 9;  // Pin connected to the servo
+Servo myServo;
+const int servoPin = 9;
 
 void setup() {
-    Serial.begin(9600);  // Start serial communication at 9600 baud
-    myServo.attach(servoPin);  // Attach the servo to the specified pin
+    Serial.begin(9600);
+    myServo.attach(servoPin);
 }
 
 void loop() {
     if (Serial.available()) {
-        // Read a byte from the serial port
         byte command = Serial.read();
-
-        // Check the command received and take action
-        if (command == 1) {  // If command is 1, turn left
-            myServo.write(0);  // Adjust this angle for your servo
-        } else if (command == 2) {  // If command is 2, turn right
-            myServo.write(180);  // Adjust this angle for your servo
+        if (command == 1) {
+            myServo.write(0);
+        } else if (command == 2) {
+            myServo.write(180);
         }
         else if (command == 0){
-            myServo.write(120);  // Adjust this angle for your servo
+            myServo.write(120);
         }
-        // You can add more commands if needed
     }
 }
